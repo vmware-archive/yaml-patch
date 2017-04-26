f13f0651de31874ad1088188ef23226c2640d70d
Kris Hicks <krishicks@gmail.com>
2017-04-26 17:24:49 -0600

    Initial commit
    This includes most of the spec:
    
      add
      remove
      replace
      move
      copy
    
    There are two tests around add that need to be looked at, and the test op needs
    to be implemented.
    
    Also, I haven't added tests for cases that should fail yet.

---
+++ patch.go
@@ -0,0 +1,465 @@
+package yamlpatch
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+
+	yaml "gopkg.in/yaml.v2"
+)
+
+const (
+	eRaw = iota
+	eDoc
+	eAry
+)
+
+const unknown string = "unknown"
+
+type lazyNode struct {
+	raw   *interface{}
+	doc   partialDoc
+	ary   partialArray
+	which int
+}
+
+func newLazyNode(raw *interface{}) *lazyNode {
+	return &lazyNode{raw: raw, which: eRaw}
+}
+
+func (n *lazyNode) MarshalYAML() (interface{}, error) {
+	switch n.which {
+	case eRaw:
+		if n.raw == nil {
+			panic(fmt.Sprintf("type is raw but raw is nil: %p", n))
+		}
+		return *n.raw, nil
+	case eDoc:
+		return n.doc, nil
+	case eAry:
+		return n.ary, nil
+	default:
+		return nil, fmt.Errorf("Unknown type")
+	}
+}
+
+func (n *lazyNode) UnmarshalYAML(unmarshal func(interface{}) error) error {
+	var data interface{}
+
+	err := unmarshal(&data)
+	if err != nil {
+		return err
+	}
+
+	n.raw = &data
+	n.which = eRaw
+	return nil
+}
+
+func (n *lazyNode) intoDoc() (*partialDoc, error) {
+	if n.which == eDoc {
+		return &n.doc, nil
+	}
+
+	raw := *n.raw
+
+	switch rt := raw.(type) {
+	case map[interface{}]interface{}:
+		doc := map[interface{}]*lazyNode{}
+
+		for k := range rt {
+			v := rt[k]
+			doc[k] = newLazyNode(&v)
+		}
+
+		n.doc = doc
+		n.which = eDoc
+		return &n.doc, nil
+	default:
+		return nil, fmt.Errorf("don't know how to convert %T into doc", raw)
+	}
+}
+
+func (n *lazyNode) intoAry() (*partialArray, error) {
+	if n.which == eAry {
+		return &n.ary, nil
+	}
+
+	raw := *n.raw
+
+	switch rt := raw.(type) {
+	case []interface{}:
+		array := make(partialArray, len(rt))
+
+		for i := range rt {
+			array[i] = newLazyNode(&rt[i])
+		}
+
+		n.ary = array
+		n.which = eAry
+		return &n.ary, nil
+	default:
+		return nil, fmt.Errorf("don't know how to convert %T into ary", raw)
+	}
+}
+
+type operation struct {
+	Op       string       `yaml:"op,omitempty"`
+	Path     string       `yaml:"path,omitempty"`
+	From     string       `yaml:"from,omitempty"`
+	RawValue *interface{} `yaml:"value,omitempty"`
+}
+
+func (o operation) value() *lazyNode {
+	if o.RawValue == nil {
+		panic(fmt.Sprintf("value is nil: %#v", o))
+	}
+	return newLazyNode(o.RawValue)
+}
+
+// Patch is an ordered collection of operations.
+type Patch []operation
+
+type container interface {
+	get(key string) (*lazyNode, error)
+	set(key string, val *lazyNode) error
+	add(key string, val *lazyNode) error
+	remove(key string) error
+}
+
+func isArray(iface interface{}) bool {
+	_, ok := iface.([]interface{})
+	return ok
+}
+
+func findObject(c *container, path string) (container, string) {
+	doc := *c
+
+	split := strings.Split(path, "/")
+
+	if len(split) < 2 {
+		return nil, ""
+	}
+
+	parts := split[1 : len(split)-1]
+
+	key := split[len(split)-1]
+
+	for _, part := range parts {
+		next, err := doc.get(decodePatchKey(part))
+		if next == nil || err != nil {
+			return nil, ""
+		}
+
+		if isArray(*next.raw) {
+			doc, err = next.intoAry()
+			if err != nil {
+				return nil, ""
+			}
+		} else {
+			doc, err = next.intoDoc()
+			if err != nil {
+				return nil, ""
+			}
+		}
+	}
+
+	return doc, decodePatchKey(key)
+}
+
+type partialDoc map[interface{}]*lazyNode
+
+func (d *partialDoc) set(key string, val *lazyNode) error {
+	(*d)[key] = val
+	return nil
+}
+
+func (d *partialDoc) add(key string, val *lazyNode) error {
+	(*d)[key] = val
+	return nil
+}
+
+func (d *partialDoc) get(key string) (*lazyNode, error) {
+	return (*d)[key], nil
+}
+
+func (d *partialDoc) remove(key string) error {
+	_, ok := (*d)[key]
+	if !ok {
+		return fmt.Errorf("Unable to remove nonexistent key: %s", key)
+	}
+
+	delete(*d, key)
+	return nil
+}
+
+type partialArray []*lazyNode
+
+func (d *partialArray) set(key string, val *lazyNode) error {
+	// if key == "-" {
+	// 	*d = append(*d, val)
+	// 	return nil
+	// }
+
+	idx, err := strconv.Atoi(key)
+	if err != nil {
+		return err
+	}
+
+	sz := len(*d)
+	if idx+1 > sz {
+		sz = idx + 1
+	}
+
+	ary := make([]*lazyNode, sz)
+
+	cur := *d
+
+	copy(ary, cur)
+
+	if idx >= len(ary) {
+		return fmt.Errorf("Unable to access invalid index: %d", idx)
+	}
+
+	ary[idx] = val
+
+	*d = ary
+	return nil
+}
+
+func (d *partialArray) add(key string, val *lazyNode) error {
+	if key == "-" {
+		*d = append(*d, val)
+		return nil
+	}
+
+	idx, err := strconv.Atoi(key)
+	if err != nil {
+		return err
+	}
+
+	ary := make([]*lazyNode, len(*d)+1)
+
+	cur := *d
+
+	copy(ary[0:idx], cur[0:idx])
+	ary[idx] = val
+	copy(ary[idx+1:], cur[idx:])
+
+	*d = ary
+	return nil
+}
+
+func (d *partialArray) get(key string) (*lazyNode, error) {
+	idx, err := strconv.Atoi(key)
+	if err != nil {
+		return nil, err
+	}
+
+	if idx >= len(*d) {
+		return nil, fmt.Errorf("Unable to access invalid index: %d", idx)
+	}
+
+	return (*d)[idx], nil
+}
+
+func (d *partialArray) remove(key string) error {
+	idx, err := strconv.Atoi(key)
+	if err != nil {
+		return err
+	}
+
+	cur := *d
+
+	if idx >= len(cur) {
+		return fmt.Errorf("Unable to remove invalid index: %d", idx)
+	}
+
+	ary := make([]*lazyNode, len(cur)-1)
+
+	copy(ary[0:idx], cur[0:idx])
+	copy(ary[idx:], cur[idx+1:])
+
+	*d = ary
+	return nil
+
+}
+
+func add(doc *container, op operation) error {
+	con, key := findObject(doc, op.Path)
+
+	if con == nil {
+		return fmt.Errorf("yamlpatch add operation does not apply: doc is missing path: %s", op.Path)
+	}
+
+	return con.add(key, op.value())
+}
+
+func remove(doc *container, op operation) error {
+	con, key := findObject(doc, op.Path)
+
+	if con == nil {
+		return fmt.Errorf("yamlpatch remove operation does not apply: doc is missing path: %s", op.Path)
+	}
+
+	return con.remove(key)
+}
+
+func replace(doc *container, op operation) error {
+	con, key := findObject(doc, op.Path)
+
+	if con == nil {
+		return fmt.Errorf("yamlpatch replace operation does not apply: doc is missing path: %s", op.Path)
+	}
+
+	val, err := con.get(key)
+	if val == nil || err != nil {
+		return fmt.Errorf("yamlpatch replace operation does not apply: doc is missing key: %s", op.Path)
+	}
+
+	return con.set(key, op.value())
+}
+
+func move(doc *container, op operation) error {
+	con, key := findObject(doc, op.From)
+	if con == nil {
+		return fmt.Errorf("yamlpatch move operation does not apply: doc is missing from path: %s", op.From)
+	}
+
+	val, err := con.get(key)
+	if err != nil {
+		return err
+	}
+
+	err = con.remove(key)
+	if err != nil {
+		return err
+	}
+
+	con, key = findObject(doc, op.Path)
+	if con == nil {
+		return fmt.Errorf("yamlpatch move operation does not apply: doc is missing destination path: %s", op.Path)
+	}
+
+	return con.set(key, val)
+}
+
+// func (p Patch) test(doc *container, op operation) error {
+// 	path := op.path()
+
+// 	con, key := findObject(doc, path)
+
+// 	if con == nil {
+// 		return fmt.Errorf("yamlpatch test operation does not apply: is missing path: %s", path)
+// 	}
+
+// 	val, err := con.get(key)
+
+// 	if err != nil {
+// 		return err
+// 	}
+
+// 	if val == nil {
+// 		if op.value().raw == nil {
+// 			return nil
+// 		}
+// 		return fmt.Errorf("Testing value %s failed", path)
+// 	}
+
+// 	if val.equal(op.value()) {
+// 		return nil
+// 	}
+
+// 	return fmt.Errorf("Testing value %s failed", path)
+// }
+
+func copyOp(doc *container, op operation) error {
+	con, key := findObject(doc, op.From)
+	if con == nil {
+		return fmt.Errorf("copy operation does not apply: doc is missing from path: %s", op.From)
+	}
+
+	val, err := con.get(key)
+	if err != nil {
+		return err
+	}
+
+	con, key = findObject(doc, op.Path)
+	if con == nil {
+		return fmt.Errorf("copy operation does not apply: doc is missing destination path: %s", op.Path)
+	}
+
+	return con.set(key, val)
+}
+
+// DecodePatch decodes the passed YAML document as if it were an RFC 6902 patch
+func DecodePatch(bs []byte) (Patch, error) {
+	var p Patch
+
+	err := yaml.Unmarshal(bs, &p)
+	if err != nil {
+		return nil, err
+	}
+
+	return p, nil
+}
+
+// Apply returns a YAML document that has been mutated per the patch
+func (p Patch) Apply(doc []byte) ([]byte, error) {
+	var c container
+
+	c = &partialDoc{}
+	err := yaml.Unmarshal(doc, c)
+	if err != nil {
+		c = &partialArray{}
+		err = yaml.Unmarshal(doc, c)
+		if err != nil {
+			return nil, fmt.Errorf("failed unmarshaling doc: %s\n\n%s", string(doc), err)
+		}
+	}
+
+	for _, op := range p {
+		switch op.Op {
+		case "add":
+			err = add(&c, op)
+		case "remove":
+			err = remove(&c, op)
+		case "replace":
+			err = replace(&c, op)
+		case "move":
+			err = move(&c, op)
+		case "copy":
+			err = copyOp(&c, op)
+		// 	// case "test":
+		// 	// 	err = p.test(&c, op)
+		default:
+			err = fmt.Errorf("Unexpected op: %s", op.Op)
+		}
+
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return yaml.Marshal(c)
+}
+
+// From http://tools.ietf.org/html/rfc6901#section-4 :
+//
+// Evaluation of each reference token begins by decoding any escaped
+// character sequence.  This is performed by first transforming any
+// occurrence of the sequence '~1' to '/', and then transforming any
+// occurrence of the sequence '~0' to '~'.
+
+var (
+	rfc6901Encoder = strings.NewReplacer("~", "~0", "/", "~1")
+	rfc6901Decoder = strings.NewReplacer("~1", "/", "~0", "~")
+)
+
+func decodePatchKey(k string) string {
+	return rfc6901Decoder.Replace(k)
+}
+
+func encodePatchKey(k string) string {
+	return rfc6901Encoder.Replace(k)
+}
---
+++ patch_test.go
@@ -0,0 +1,114 @@
+package yamlpatch_test
+
+import (
+	yamlpatch "github.com/krishicks/yaml-patch"
+	yaml "gopkg.in/yaml.v2"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/ginkgo/extensions/table"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Patch", func() {
+	DescribeTable(
+		"positive cases",
+		func(doc, ops, expectedYAML string) {
+			patch, err := yamlpatch.DecodePatch([]byte(ops))
+			Expect(err).NotTo(HaveOccurred())
+
+			actualBytes, err := patch.Apply([]byte(doc))
+			Expect(err).NotTo(HaveOccurred())
+
+			var actualIface interface{}
+			err = yaml.Unmarshal(actualBytes, &actualIface)
+			Expect(err).NotTo(HaveOccurred())
+
+			var expectedIface interface{}
+			err = yaml.Unmarshal([]byte(expectedYAML), &expectedIface)
+			Expect(err).NotTo(HaveOccurred())
+
+			Expect(actualIface).To(Equal(expectedIface))
+		},
+		Entry("adding an element to an object",
+			"---\nfoo: bar",
+			"---\n- op: add\n  path: /baz\n  value: qux",
+			"---\nfoo: bar\nbaz: qux",
+		),
+		Entry("adding an element to an array",
+			"---\nfoo: [bar,baz]",
+			"---\n- op: add\n  path: /foo/1\n  value: qux",
+			"---\nfoo: [bar,qux,baz]",
+		),
+		Entry("removing an element from an object",
+			"---\nfoo: bar\nbaz: qux",
+			"---\n- op: remove\n  path: /baz",
+			"---\nfoo: bar",
+		),
+		Entry("removing an element from an array",
+			"---\nfoo: [bar,qux,baz]",
+			"---\n- op: remove\n  path: /foo/1",
+			"---\nfoo: [bar,baz]",
+		),
+		Entry("replacing an element in an object",
+			"---\nfoo: bar\nbaz: qux",
+			"---\n- op: replace\n  path: /baz\n  value: boo",
+			"---\nfoo: bar\nbaz: boo",
+		),
+		Entry("moving an element in an object",
+			"---\nfoo:\n  bar: baz\n  waldo: fred\nqux:\n  corge: grault",
+			"---\n- op: move\n  from: /foo/waldo\n  path: /qux/thud",
+			"---\nfoo:\n  bar: baz\nqux:\n  corge: grault\n  thud: fred",
+		),
+		Entry("moving an element in an array",
+			"---\nfoo: [all, grass, cows, eat]",
+			"---\n- op: move\n  from: /foo/1\n  path: /foo/3",
+			"---\nfoo: [all, cows, eat, grass]",
+		),
+		Entry("adding an object to an object",
+			"---\nfoo: bar",
+			"---\n- op: add\n  path: /child\n  value:\n    grandchild: {}",
+			"---\nfoo: bar\nchild:\n  grandchild: {}",
+		),
+		Entry("appending an element to an array",
+			"---\nfoo: [bar]",
+			"---\n- op: add\n  path: /foo/-\n  value: [abc,def]",
+			"---\nfoo: [bar, [abc, def]]",
+		),
+		Entry("removing a nil element from an object",
+			"---\nfoo: bar\nqux:\n  baz: 1\n  bar: ~",
+			"---\n- op: remove\n  path: /qux/bar",
+			"---\nfoo: bar\nqux:\n  baz: 1",
+		),
+		Entry("adding a nil element to an object",
+			"---\nfoo: bar",
+			"---\n- op: add\n  path: /baz\n  value: ~",
+			"---\nfoo: bar\nbaz: ~",
+		),
+		Entry("replacing the sole element in an array",
+			"---\nfoo: [bar]",
+			"---\n- op: replace\n  path: /foo/0\n  value: baz",
+			"---\nfoo: [baz]",
+		),
+		Entry("replacing an element in an array within a root array",
+			"---\n- foo: [bar, qux, baz]",
+			"---\n- op: replace\n  path: /0/foo/0\n  value: bum",
+			"---\n- foo: [bum, qux, baz]",
+		),
+		Entry("copying an element in an array within a root array with an index",
+			"---\n- foo: [bar, qux, baz]\n  bar: [qux, baz]",
+			"---\n- op: copy\n  from: /0/foo/0\n  path: /0/bar/0",
+			"---\n- foo: [bar, qux, baz]\n  bar: [bar, baz]",
+		),
+		XEntry("copying an element in an array within a root array to a destination without an index",
+			// this is in jsonpatch, but I'd like confirmation from the spec that this is intended
+			"---\n- foo: [bar, qux, baz]\n  bar: [qux, baz]",
+			"---\n- op: copy\n  from: /0/foo/0\n  path: /0/bar",
+			"---\n- foo: [bar, qux, baz]\n  bar: [bar, qux, baz]",
+		),
+		XEntry("copying an element in an array within a root array to a destination without an index",
+			"---\n- foo:\n  bar: [qux, baz]\n  baz:\n    qux: bum",
+			"---\n- op: copy\n  from: /0/foo/bar\n  path: /0/baz/bar",
+			"---\n- foo: [bar, qux, baz]\n  bar: [bar, qux, baz]",
+		),
+	)
+})
